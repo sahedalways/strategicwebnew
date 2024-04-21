55f1ebe0333b2f9ba3cd3c584d7f2296e5c317cb
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2024-04-21 17:52:34 +0600

    updated

--- resources/views/frontend/press.blade.php
+++ resources/views/frontend/press.blade.php
@@ -18,7 +18,19 @@
                          <div class="col-md-4 p-4 bg-light shadow-sm">
                              <div style="font-size: 12px;" class="text-secondary mb-3">PRESS RELEASE</div>
                              <h2 class="fw-semibold">{{ $article->title }}</h2>
+                             <p class="press-author">{{ $article->author }}</p>
+                             <p class="press-desc">
+                                 {!! substr(html_entity_decode($article->description), 0, 150) !!}
+                                 {!! strlen($article->description) > 150 ? '...' : '' !!}
+                             </p>
+                             <div class="share-icons">
+                                 <a href="#"
+                                     onclick="shareToSocialMedia('{{ route('press-details', ['id' => $article->id]) }}')"
+                                     class="share-icon"><i class="fa fa-share"></i></a>
+                             </div>
                          </div>
+                         <div style="font-size: 12px;" class="text-secondary mt-5 fw-semibold">
+                             {{ $article->created_at->format('F d, Y') }}</div>
                      </a>
                  </div>
              @endif
@@ -42,7 +54,17 @@
                                          <div style="font-size: 12px;" class="text-secondary mb-2 fw-semibold">PHOTOS</div>
 
                                          <h4 class="fw-semibold">{{ $article->title }}</h4>
+                                         <p class="press-author">{{ $article->author }}</p>
+                                         <p class="press-desc">
+                                             {!! substr(html_entity_decode($article->description), 0, 150) !!}
+                                             {!! strlen($article->description) > 150 ? '...' : '' !!}
+                                         </p>
 
+                                         <div class="share-icons">
+                                             <a href="#"
+                                                 onclick="shareToSocialMedia('{{ route('press-details', ['id' => $article->id]) }}')"
+                                                 class="share-icon"><i class="fa fa-share"></i></a>
+                                         </div>
                                          <div style="font-size: 12px;" class="text-secondary mt-5 fw-semibold">
                                              {{ $article->created_at->format('F d, Y') }}</div>
                                      </div>
@@ -73,6 +95,16 @@
                                      <div class="content-sec p-4">
                                          <div style="font-size: 12px;" class="text-secondary mb-2 fw-semibold">Update</div>
                                          <h4 class="fw-semibold">{{ $article->title }}</h4>
+                                         <p class="press-author">{{ $article->author }}</p>
+                                         <p class="press-desc">
+                                             {!! substr(html_entity_decode($article->description), 0, 150) !!}
+                                             {!! strlen($article->description) > 150 ? '...' : '' !!}
+                                         </p>
+                                         <div class="share-icons">
+                                             <a href="#"
+                                                 onclick="shareToSocialMedia('{{ route('press-details', ['id' => $article->id]) }}')"
+                                                 class="share-icon"><i class="fa fa-share"></i></a>
+                                         </div>
                                          <div style="font-size: 12px;" class="text-secondary mt-5 fw-semibold">
                                              {{ $article->created_at->format('F d, Y') }}</div>
                                      </div>
@@ -84,5 +116,31 @@
              </div>
          </div>
      </div>
+
+ @endsection
+
+ @section('script')
+     <script>
+         function shareToSocialMedia(url) {
+             // Show a modal or dropdown menu for users to choose social media platform
+             // For simplicity, let's use window.prompt to ask for user input
+             var platform = window.prompt('Share to which platform? (facebook, twitter, email)');
+
+             // Based on user input, open the corresponding share URL
+             if (platform) {
+                 platform = platform.toLowerCase(); // Convert input to lowercase
+
+                 if (platform === 'facebook') {
+                     window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, '_blank');
+                 } else if (platform === 'twitter') {
+                     window.open('https://twitter.com/share?url=' + url, '_blank');
+                 } else if (platform === 'email') {
+                     window.open('mailto:?body=' + url, '_blank');
+                 } else {
+                     alert('Invalid platform. Please choose facebook, twitter, or email.');
+                 }
+             }
+         }
+     </script>
 
  @endsection
--- resources/views/frontend/press_details.blade.php
+++ resources/views/frontend/press_details.blade.php
@@ -2,20 +2,24 @@
   @section('title', 'Strategic Web' . ' - Press Details')
   @section('content')
 
-      <div class="container my-4 col-lg-9 mx-auto">
-          <div style="text-decoration: none;"
-              class="mx-auto row text-dark rounded-4 overflow-hidden p-0 shadow-sm img-hover-effect">
-              <div class="col-md-8 p-0 img-sec">
-                  <img src="{{ asset('images/article' . "/{$article->id}-1.{$article->image}") }}" alt="article-image"
-                      class="w-100">
-              </div>
-              <div class="col-md-4 p-4 bg-light shadow-sm">
-                  <div style="font-size: 12px;" class="text-secondary mb-3">PRESS RELEASE</div>
 
-                  <h2 class="fw-semibold">{{ $article->title }}</h2>
-                  <p class="fw-semibold">{{ $article->description }}</p>
+      <div class="container">
+          <div class="">
+              <div class="mt-5">
+
+                  <img src="{{ asset('images/article' . "/{$article->id}-1.{$article->image}") }}" class="img-fluid mb-4"
+                      alt="Post Image">
+
+                  <h1 class=" mb-4">{{ $article->title }}</h1>
+                  <p>{{ $article->author }}</p>
+                  <p class="text-muted">Posted on {{ $article->created_at->format('F d, Y') }}</p>
+                  <div class="post-content">
+                      <p>{!! html_entity_decode($article->description) !!}</p>
+                  </div>
               </div>
+
           </div>
+
       </div>
 
   @endsection
