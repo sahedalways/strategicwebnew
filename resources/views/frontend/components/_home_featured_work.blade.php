f3fa3d1fd374a6794d3c07f6fa90648fd1d7cc94
Tarek Mahmud <72188853+mahmud4020@users.noreply.github.com>
2024-04-25 15:01:43 +0600

    image uploded

--- resources/views/frontend/components/_home_featured_work.blade.php
+++ resources/views/frontend/components/_home_featured_work.blade.php
@@ -6,26 +6,26 @@
         <h4 class="pb-4 mt-0 fw-semibold">Graphic Design</h4>
           <div class="row">
               <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-                  <img style="cursor: pointer;" src="{{ asset('images/FlyerDesign/FlyerDesign-1.png') }}"
+                  <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (1).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Boat on Calm Water" />
 
-                      <img style="cursor: pointer;" src="{{ asset('images/Social Media Kit/Social Media Kit-1.png') }}"
+                      <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (5).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Yosemite National Park" />
               </div>
 
               <div class="col-lg-4 mb-4 mb-lg-0">
-                  <img style="cursor: pointer;" src="{{ asset('images/Logo Design/Logo 1.jpg') }}"
+                  <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (3).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Mountains in the Clouds" />
 
-                  <img style="cursor: pointer;" src="{{ asset('images/Landing Page Design/Landing Page Design 5-01.png') }}"
+                  <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (2).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Boat on Calm Water" />
               </div>
 
               <div class="col-lg-4 mb-4 mb-lg-0">
-                  <img style="cursor: pointer;" src="{{ asset('images/FlyerDesign/Medical Flyer Design 1.png') }}"
+                  <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (12).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Waves at Sea" />
 
-                  <img style="cursor: pointer;" src="{{ asset('images/Social Media Kit/Social Media Kit-2.png') }}"
+                  <img style="cursor: pointer;" src="{{ asset('images/graphic/graphic (7).jpg') }}"
                       class="w-100 shadow-1-strong rounded mb-4 gallery-img" alt="Yosemite National Park" />
               </div>
           </div>
@@ -44,33 +44,39 @@
       <div class="owl-carousel">
           <div class="item">
               <img class="image-1"
-                  src="{{ asset('images/FlyerDesign/Flyer Design-2.png') }}"
+                  src="{{ asset('images/graphic/graphic (1).jpg') }}"
                   alt="Image 1">
-              <img class="image-2" src="{{ asset('images/FlyerDesign/Medical Flyer Design 1.png') }}" alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (2).jpg') }}" alt="Image 1">
           </div>
 
           <div class="item">
               <img class="image-1"
-                  src="{{ asset('images/Landing Page Design/Landing Page Design 3-01.png') }}"
+                  src="{{ asset('images/graphic/graphic (3).jpg') }}"
                   alt="Image 1">
-              <img class="image-2" src="{{ asset('images/Landing Page Design/Landing Page Design 5-01.png') }}" alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (4).jpg') }}" alt="Image 1">
           </div>
 
           <div class="item">
-              <img class="image-1" src="{{ asset('images/Logo Design/Logo 2.png') }}" alt="Image 1">
-              <img class="image-2" src="{{ asset('images/Logo Design/Logo 1.jpg') }}" alt="Image 1">
+              <img class="image-1" src="{{ asset('images/graphic/graphic (5).jpg') }}" alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (6).jpg') }}" alt="Image 1">
           </div>
           <div class="item">
               <img class="image-1"
-                  src="{{ asset('images/Social Media Kit/Social Media Kit-01.png') }}"
+                  src="{{ asset('images/graphic/graphic (7).jpg') }}"
                   alt="Image 1">
-              <img class="image-2" src="{{ asset('images/Social Media Kit/Social Media Kit-1.png') }}" alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (8).jpg') }}" alt="Image 1">
           </div>
           <div class="item">
               <img class="image-1"
-                  src="{{ asset('images/Social Media Kit/Social Media Kit-02.png') }}"
+                  src="{{ asset('images/graphic/graphic (10).jpg') }}"
                   alt="Image 1">
-              <img class="image-2" src="{{ asset('images/Social Media Kit/Social Media Kit-2.png') }}" alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (11).jpg') }}" alt="Image 1">
+          </div>
+          <div class="item">
+              <img class="image-1"
+                  src="{{ asset('images/graphic/graphic (12).jpg') }}"
+                  alt="Image 1">
+              <img class="image-2" src="{{ asset('images/graphic/graphic (13).jpg') }}" alt="Image 1">
           </div>
 
 
